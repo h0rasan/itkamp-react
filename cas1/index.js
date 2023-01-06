9f3fde9314e504bc46e55569802f27b24ad815eb
Seid Jasarevic <jasarevics@live.com>
2023-01-06 19:57:18 +0100

    first commit

---
+++ cas1/index.js
@@ -0,0 +1,42 @@
+// const je
+// var je
+// let je
+//destrukturizacija objekta = object destruction
+// raxzlaganje objekta na zasebne promenjljive
+// const randomObj = {
+//     name: 'Seid',
+//     lName: 'Jasarevic',
+//     age: 0,
+//     city: 'Novi Pazar'
+// }
+
+// const newObj = {
+//     ...randomObj
+//     age = 27
+// }
+
+// console.log(newObj);
+
+// if (newObj.age === 27) {
+//     console.log(newObj);
+// } else { "error" }
+
+// newObj.age === 27 ? console.log(newObj) : console.log('error');
+
+// = lijeva strana preuzima vrijednost desne
+// == uporedjuje svrijednost dvije strane
+// === uporedjuje vrijednost i tip dvije strane
+// class based components su komponente bazirane na klasama i metodama
+// klase(class) je izrada modela podataka unutar aplikacije(web ili bilo koje druge)
+// Svaka klasa mora da ima metodu constructor(){} a ona vrsi funkciju glavne funkcije(metode) unutar klase
+// pomocu constuctor motode se inicijalizuju glavni podaci unutar klase
+// inicijalizacija promenljivih i dodela vrijednosti njima vesi se pomocu this mmetode (npr this.name)
+// metode su funkcjie unutar klase koje takodje mogu vrsiti inicijalizaciju promenljivih i obradu podataka
+// Svaka klasa mora imati constructor metodu
+
+// Node je runtime koji vrsi egzekuciju i kompajlovanje javacript koda
+// Node ima mnostvo svojih modula najpoznatiji su fs(fajlsistem) i https modul
+
+// cd - change directory
+// ls - izlistavanje sadrzaja foldera
+// mkdir - make directory
---
+++ cas3/index.js
@@ -0,0 +1,13 @@
+class Car {
+  constructor(carName, carType) {
+    this.name = carName;
+    this.type = carType;
+  }
+  showAttr() {
+    console.log(this.name, this.type);
+  }
+}
+
+const newCar = new Car('audi', 'limo');
+
+newCar.showAttr();
