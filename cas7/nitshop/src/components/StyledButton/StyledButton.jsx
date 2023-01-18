c6d840588f54ee54a6d2427a8ecff0c92c81a3fc
Seid Jasarevic <jasarevics@live.com>
2023-01-18 20:36:04 +0100

    ReactHooks

--- cas7/nitshop/src/components/StyledButton/StyledButton.jsx
+++ cas7/nitshop/src/components/StyledButton/StyledButton.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import './StyledButton.css';
 
-const StyledButton = () => {
-  var buttonText = 'Click me';
-
-  function buttonHandler() {
-    buttonText = 'Clicked!';
-    alert('Clicked!');
-    return buttonText;
-  }
+const StyledButton = (props) => {
   return (
-    <button className='styledButton' onClick={() => buttonHandler()}>
-      {buttonText}
+    <button className='styledButton' onClick={props.onClickHandler}>
+      {props.innerText}
     </button>
   );
 };
